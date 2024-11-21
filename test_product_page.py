3226dd2774616a3bf9e5e8a495b85d6a60dcd85c
EKF <kostenkovan@yandex.ru>
2024-11-21 10:43:23 +0300

    Create 3 new def in page_product

--- test_product_page.py
+++ test_product_page.py
@@ -2,7 +2,7 @@ from time import sleep
 from .pages.product_page import ProductPage
 import pytest
 
-
+@pytest.mark.skip
 @pytest.mark.parametrize('link', [0, 1, 2, 3, 4, 5, 6,
                                   pytest.param(7, marks=pytest.mark.xfail),
                                   8, 9])
@@ -14,3 +14,25 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.solve_quiz_and_get_code()
     page.check_name_add_in_basket()
     sleep(2)
+
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_in_basket()
+    page.solve_quiz_and_get_code()
+    page.should_not_be_success_message()
+
+def test_guest_cant_see_success_message(browser):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_in_basket()
+    page.solve_quiz_and_get_code()
+    page.should_be_success_message()
